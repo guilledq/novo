1aeae34f0208fdbb61f4b2cde5e9ba2bfa478e09
Guillermo Quiles <92491666+guilledq@users.noreply.github.com>
2022-04-22 19:38:04 -0300

    Update practica.js
    abril

--- practica.js
+++ practica.js
@@ -7,3 +7,4 @@ console.log (age)
 console.log (name + " " + lastName + " tiene " + " " + age + " años ")
 var age = age + 1 // agregamos un año mas
 console.log (name + " " + lastName + " tiene " + " " + age + " años ")
+2022
