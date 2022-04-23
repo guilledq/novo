67e8d494683fb5c58c4eb7e0493751e753136197
guilledq <quiles_guille10@hotmail.com>
2022-04-22 22:10:09 -0300

    good night

--- abril.js
+++ abril.js
@@ -1,1 +1,23 @@
-const  age = 26
+//const  age = 26
+//if (age > 18){
+//    console.log("Es mayor de edad")
+//} else if (age < 18){
+//    console.log("Es menor de edad")
+//}
+
+//const a = 12
+//const b = 30
+//const suma = a+b
+//if (suma > a){
+//    console.log("Es mayor a a")
+//}
+//if (suma > b){
+//    console.log("Es mayor a b")
+//}
+
+const numero = 30
+if (numero < 31){
+    console.log("Es par")
+} else if (numero > 29){
+    console.log("Es impar")
+}
